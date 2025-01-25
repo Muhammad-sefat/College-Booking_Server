e79a6ba6a055d6d4a4adf2164cbd4d106c329b7f
Muhammad Sefat <muhammadsefat55@gmail.com>
2025-01-25 20:53:09 +0600

    Final commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://collage-booking-client.vercel.app",
+    origin: ["https://collage-booking-client.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
